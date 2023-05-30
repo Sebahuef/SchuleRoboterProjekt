885c62a94d209905f80049f9a762133d272f35c1
Felix-Fab <69577380+Felix-Fab@users.noreply.github.com>
2023-05-30 11:14:42 +0200

    Update main.py

--- Roboter/main.py
+++ Roboter/main.py
@@ -1,14 +1,30 @@
 import DoBotArm as Dbt
+from paho.mqtt import client as mqtt_client
+
+Color = "Blue"
     
 def main():
     homeX, homeY, homeZ = 257, 4, 76, 
-    ctrlBot = Dbt.DoBotArm(homeX, homeY, homeZ) #Create DoBot Class Object with home position x,y,z
-    ctrlBot.toggleSuction()
-    ctrlBot.moveArmXYZ(286,12,-36)
+    ctrlBot = Dbt.DoBotArm(homeX, homeY, homeZ)
     
-    ctrlBot.moveHome()
+    while True:
+        Color = input("Farbe: ")
+        
+        
+        ctrlBot.toggleSuction()
+        ctrlBot.moveArmXYZ(286,12,-36)
     
-    ctrlBot.moveArmXYZ(-6,-270,125)
-    ctrlBot.toggleSuction()
+        ctrlBot.moveHome()
+    
+        if Color == "Red":
+            ctrlBot.moveArmXYZ(182,190,-29)
+        elif Color == "Blue":
+            ctrlBot.moveArmXYZ(113,242,-32)
+        else:
+            ctrlBot.moveArmXYZ(39,275,-33)
+        
+        ctrlBot.toggleSuction()
+        ctrlBot.moveHome()
 
 main()
+
