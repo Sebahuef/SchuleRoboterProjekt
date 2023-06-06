3bf649de94da3e858e7df14b53103e3a33157002
Felix-Fab <69577380+Felix-Fab@users.noreply.github.com>
2023-06-06 11:27:57 +0200

    Update main.py

--- Roboter/main.py
+++ Roboter/main.py
@@ -1,30 +1,72 @@
 import DoBotArm as Dbt
 from paho.mqtt import client as mqtt_client
+import random
+import json
 
-Color = "Blue"
+Color = None
+
+broker = '10.100.20.142'
+port = 1883
+topic = "CubeColor"
+
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
     
 def main():
+    client = connect_mqtt()
+    client.loop_start()
+    
+    print(Color)
+    
     homeX, homeY, homeZ = 257, 4, 76, 
     ctrlBot = Dbt.DoBotArm(homeX, homeY, homeZ)
     
     while True:
-        Color = input("Farbe: ")
         
+        if Color != None:
         
-        ctrlBot.toggleSuction()
-        ctrlBot.moveArmXYZ(286,12,-36)
+            input("Start:")
+        
+            ctrlBot.toggleSuction()
+            ctrlBot.moveArmXYZ(286,12,-36)
     
-        ctrlBot.moveHome()
+            ctrlBot.moveHome()
     
-        if Color == "Red":
-            ctrlBot.moveArmXYZ(182,190,-29)
-        elif Color == "Blue":
-            ctrlBot.moveArmXYZ(113,242,-32)
+            if Color == "Red":
+                ctrlBot.moveArmXYZ(182,190,-29)
+            elif Color == "Blue":
+                ctrlBot.moveArmXYZ(113,242,-32)
+            elif Color == "Green":
+                ctrlBot.moveArmXYZ(39,275,-33)
+            else:
+                print("No Color detected")
+        
+            ctrlBot.toggleSuction()
+            ctrlBot.moveHome()
+            
         else:
-            ctrlBot.moveArmXYZ(39,275,-33)
+            print("Color None")
         
-        ctrlBot.toggleSuction()
-        ctrlBot.moveHome()
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+            
+            client.subscribe("CubeColor")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    
+    def on_message(client, userdata, msg):
+        global Color
+        
+        Data = json.loads(msg.payload)
+        Color = Data['color']
+    
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(broker, port)
+    return client
 
 main()
+
 
