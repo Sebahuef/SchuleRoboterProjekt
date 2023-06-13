eac15d288a41827ea41aac40cb1ec6ca85de391b
Felix-Fab <69577380+Felix-Fab@users.noreply.github.com>
2023-06-13 10:23:25 +0200

    Update main.py

--- Roboter/main.py
+++ Roboter/main.py
@@ -1,60 +1,55 @@
 import DoBotArm as Dbt
-from paho.mqtt import client as mqtt_client
 import random
 import json
-
-Color = None
+import socket
     
 def main():
-    client = connect_mqtt()
-    client.loop_start()
-    
-    print(Color)
-    
     homeX, homeY, homeZ = 257, 4, 76, 
     ctrlBot = Dbt.DoBotArm(homeX, homeY, homeZ)
     
     while True:
         
-        if Color != None:
-        
-            input("Start:")
+        input("Start:")
             
-            GetCurrentColor()
+        Color = GetCurrentColor()
         
-            ctrlBot.toggleSuction()
-            ctrlBot.moveArmXYZ(286,12,-36)
+        ctrlBot.toggleSuction()
+        ctrlBot.moveArmXYZ(286,12,-36)
     
-            ctrlBot.moveHome()
+        ctrlBot.moveHome()
     
-            if Color == "Red":
-                ctrlBot.moveArmXYZ(182,190,-29)
-            elif Color == "Blue":
-                ctrlBot.moveArmXYZ(113,242,-32)
-            elif Color == "Green":
-                ctrlBot.moveArmXYZ(39,275,-33)
-            else:
-                print("No Color detected")
-        
+        if Color == "red":
+            ctrlBot.moveArmXYZ(182,190,50)
+            ctrlBot.moveArmXYZ(182,190,-33)
             ctrlBot.toggleSuction()
-            ctrlBot.moveHome()
-            
+            ctrlBot.moveArmXYZ(182,190,50)
+        elif Color == "blue":
+            ctrlBot.moveArmXYZ(113,242,50)
+            ctrlBot.moveArmXYZ(113,242,-33)
+            ctrlBot.toggleSuction()
+            ctrlBot.moveArmXYZ(113,242,50)
+        elif Color == "green":
+            ctrlBot.moveArmXYZ(39,275,50)
+            ctrlBot.moveArmXYZ(39,275,-33)
+            ctrlBot.toggleSuction()
+            ctrlBot.moveArmXYZ(39,275,50)
         else:
-            print("Color None")
+            print("No Color detected")
+        
+        ctrlBot.moveHome()
             
 def get_colour_name(b_mean, g_mean, r_mean):
     currentColor = ""
-    # Bestimmt die prominenteste Farbe und setzt die Variable
     if (b_mean > g_mean and b_mean > r_mean) :
         currentColor = "blue"
     elif (g_mean > r_mean and g_mean > b_mean) :
-        currentColor = "green”"
+        currentColor = "green"
     else:
         currentColor = "red"
     return currentColor
 
 def GetCurrentColor():
-    HOST = "10.62.255.10" # The server's hostname or IP address
+    HOST = "10.62.255.10"
     PORT = 65432
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
@@ -62,22 +57,18 @@ def GetCurrentColor():
             try:
                 data = "get color"
                 s.sendall(bytes(data, "ascii"))
-                if(data=="close"):
-                    print("closing...")
-                    s.sendall(bytes("close", "ascii"))
-                    s.close()
-                    break;
-                else:
-                    data = s.recv(1024)
-                    print([data[0], data[1], data[2]])
-                    actual_name = get_colour_name(data[0], data[1], data[2])
-                    Color = actual_name
-                    return
+                data = s.recv(1024)
+                actual_name = get_colour_name(data[0], data[1], data[2])
+                s.sendall(bytes("close", "ascii"))
+                s.close()
+                
+                return actual_name
             except KeyboardInterrupt:
                 s.sendall(bytes("close", "ascii"))
                 s.close()
 
 main()
+
 
 
 
